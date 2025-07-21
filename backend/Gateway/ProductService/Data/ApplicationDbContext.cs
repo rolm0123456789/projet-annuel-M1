3dc27bb04bcd387f383eac76f0a6b153bdec18f5
rolm0123456789 <118365568+rolm0123456789@users.noreply.github.com>
2025-07-21 12:16:38 +0200

    feat: Ajout de la migration initiale pour le modèle de produit avec création de la table Products

--- backend/Gateway/ProductService/Data/ApplicationDbContext.cs
+++ backend/Gateway/ProductService/Data/ApplicationDbContext.cs
@@ -6,10 +6,4 @@ namespace ProductService.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<ProductModel> Products => Set<ProductModel>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<ProductModel>();
-    }
 }
